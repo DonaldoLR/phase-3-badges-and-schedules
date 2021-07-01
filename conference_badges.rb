663386058e92b48ccfa8c382c6b791479822544b
github username <github email address>
2021-07-01 12:13:30 -0400

    worked with arrays and used iteration in ruby

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(array) 
+  array.each_with_index.map {|name, idx| "Hello, #{name}! You'll be assigned to room #{idx + 1}!"}
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each {|badge| puts badge}
+  assign_rooms(attendees).each {|assigned_rooms| puts assigned_rooms}
+end
